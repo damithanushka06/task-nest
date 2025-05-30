05ded5b7417014433497b909336ffbfe2f906b6b
Damith Anushka <155383770+damithanushka06@users.noreply.github.com>
2025-05-30 20:38:03 +0530

    Added main component structure and updated the REDME file

---
+++ src/app/features/task-board/task-column/task-column.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-column',
+  imports: [],
+  templateUrl: './task-column.component.html',
+  styleUrl: './task-column.component.scss'
+})
+export class TaskColumnComponent {
+
+}
